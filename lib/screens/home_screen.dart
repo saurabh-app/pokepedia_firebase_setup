ef6e753c3e7e67a95f75c8b370ba75b2ecfd5938
HP <saurkumar1994@gmail.com>
2025-06-03 00:00:36 +0530

    (SKY)Details page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:pokepedia_firebase_setup/main.dart';
+
+void main() {
+  runApp(const MaterialApp(home: HomeScreen()));
+}
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,21 +15,87 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var pokepediaApi = "https://pokeapi.co/api/v2/pokemon/ditto";
   List pokepedia = [];
+  bool isLoading = true;
 
   @override
   void initState() {
-    if (mounted) {
-      fetchData();
-    }
     super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=151"));
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final List results = data['results'];
+
+        List<Future> futures = results.map((pokemon) async {
+          final detailResponse = await http.get(Uri.parse(pokemon['url']));
+          if (detailResponse.statusCode == 200) {
+            final detailData = jsonDecode(detailResponse.body);
+            return {
+              'name': detailData['name'],
+              'image': detailData['sprites']['other']['official-artwork']['front_default'] ?? '',
+              'types': detailData['types']
+                  .map<String>((t) => t['type']['name'].toString())
+                  .toList(),
+            };
+          } else {
+            return null;
+          }
+        }).toList();
+
+        final allDetails = await Future.wait(futures);
+
+        setState(() {
+          pokepedia = allDetails.where((e) => e != null).toList();
+          isLoading = false;
+        });
+      } else {
+        print("Failed to load list: ${response.statusCode}");
+        setState(() => isLoading = false);
+      }
+    } catch (e) {
+      print("Error: $e");
+      setState(() => isLoading = false);
+    }
+  }
+
+  Color _getTypeColor(String type) {
+    switch (type.toLowerCase()) {
+      case 'grass':
+        return Colors.greenAccent;
+      case 'fire':
+        return Colors.redAccent;
+      case 'water':
+        return Colors.blueAccent;
+      case 'electric':
+        return Colors.yellowAccent;
+      case 'rock':
+        return Colors.grey;
+      case 'ground':
+        return Colors.brown;
+      case 'psychic':
+        return Colors.indigo;
+      case 'fighting':
+        return Colors.orange;
+      case 'bug':
+        return Colors.lightGreen;
+      case 'ghost':
+        return Colors.deepPurple;
+      case 'normal':
+        return Colors.black26;
+      case 'poison':
+        return Colors.deepPurpleAccent;
+      default:
+        return Colors.pinkAccent;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
         children: [
@@ -40,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
               fit: BoxFit.fitHeight,
             ),
           ),
-          Positioned(
+          const Positioned(
             top: 90,
             left: 15,
             child: Text(
@@ -51,142 +119,184 @@ class _HomeScreenState extends State<HomeScreen> {
           Positioned(
             top: 160,
             bottom: 0,
-            // child: Container(height: height, width: width, color: Colors.red),
-            child: Column(
-              children: [
-                pokepedia != null
-                    ? Expanded(
-                      child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.4,
-                        ),
-                        itemBuilder: (context, index) {
-                          var type = pokepedia[index]['type'][0];
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color:
-                                    type == 'Grass'
-                                        ? Colors.greenAccent
-                                        : type == 'Fire'
-                                        ? Colors.redAccent
-                                        : type == 'Water'
-                                        ? Colors.blueAccent
-                                        : type == 'Electric'
-                                        ? Colors.yellowAccent
-                                        : type == 'Rock'
-                                        ? Colors.grey
-                                        : type == 'Ground'
-                                        ? Colors.brown
-                                        : type == 'Psychic'
-                                        ? Colors.indigo
-                                        : type == 'Fighthing'
-                                        ? Colors.orange
-                                        : type == 'Bug'
-                                        ? Colors.lightGreen
-                                        : type == 'Ghost'
-                                        ? Colors.deepPurple
-                                        : type == 'Normal'
-                                        ? Colors.black26
-                                        : type == 'Poison'
-                                        ? Colors.deepPurpleAccent
-                                        : Colors.pink,
-                              ),
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                    bottom: -10,
-                                    right: -10,
-                                    child: Image.asset(
-                                      'assets/images/pokeball.png',
-                                      width: 100,
-                                      fit: BoxFit.fitHeight,
-                                    ),
-                                  ),
-                                  Positioned(
-                                    top: 15,
-                                    left: 10,
-                                    child: Text(
-                                      pokepedia[index]['Name'],
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned(
-                                    top: 40,
-                                    left: 10,
-                                    child: Container(
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                          left: 8.0,
-                                          right: 8.0,
-                                          top: 4.0,
-                                          bottom: 4.0,
-                                        ),
-                                        child: Text(
-                                          type.toString(),
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                      ),
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(15),
-                                        ),
-                                        color: Colors.white30,
-                                      ),
-                                    ),
-                                  ),
-                                  Positioned(
-                                    bottom: 5,
-                                    right: 5,
-                                    child: CachedNetworkImage(
-                                      imageUrl: pokepedia[index]['img'],
-                                      height: 100,
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ],
-                              ),
+            left: 0,
+            right: 0,
+            child: isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : GridView.builder(
+                    padding: const EdgeInsets.all(8),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.4,
+                    ),
+                    itemCount: pokepedia.length,
+                    itemBuilder: (context, index) {
+                      final pokemon = pokepedia[index];
+                      final String name = pokemon['name'] ?? 'Unknown';
+                      final String image = pokemon['image'] ?? '';
+                      final List typeList = pokemon['types'] ?? [];
+                      final String type = typeList.isNotEmpty ? typeList[0] : 'Unknown';
+
+                      Color bgColor = _getTypeColor(type);
+
+                      return InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () {
+                          // Navigate to details page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailScreen(pokemon: pokemon),
                             ),
                           );
                         },
-                      ),
-                    )
-                    : Center(child: CircularProgressIndicator()),
-              ],
-            ),
+                        child: Container(
+                          margin: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: bgColor,
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                bottom: -10,
+                                right: -10,
+                                child: Image.asset(
+                                  'assets/images/pokeball.png',
+                                  width: 100,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                              Positioned(
+                                top: 15,
+                                left: 10,
+                                child: Text(
+                                  name[0].toUpperCase() + name.substring(1),
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                top: 40,
+                                left: 10,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    color: Colors.white30,
+                                  ),
+                                  child: Text(
+                                    type,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 5,
+                                right: 5,
+                                child: CachedNetworkImage(
+                                  imageUrl: image,
+                                  height: 90,
+                                  fit: BoxFit.fill,
+                                  placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
           ),
         ],
       ),
     );
-    // return Scaffold(
-    //   body: Center(
-    //     child: ElevatedButton(
-    //       child: Text("Button"),
-    //       onPressed: () {
-    //         // print("Pressed");
-    //         fetchData();
-    //       },
-    //     ),
-    //   ),
-    // );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final Map pokemon;
+  const DetailScreen({super.key, required this.pokemon});
+
+  Color _getTypeColor(String type) {
+    switch (type.toLowerCase()) {
+      case 'grass':
+        return Colors.greenAccent;
+      case 'fire':
+        return Colors.redAccent;
+      case 'water':
+        return Colors.blueAccent;
+      case 'electric':
+        return Colors.yellowAccent;
+      case 'rock':
+        return Colors.grey;
+      case 'ground':
+        return Colors.brown;
+      case 'psychic':
+        return Colors.indigo;
+      case 'fighting':
+        return Colors.orange;
+      case 'bug':
+        return Colors.lightGreen;
+      case 'ghost':
+        return Colors.deepPurple;
+      case 'normal':
+        return Colors.black26;
+      case 'poison':
+        return Colors.deepPurpleAccent;
+      default:
+        return Colors.pinkAccent;
+    }
   }
 
-  void fetchData() {
-    var url = Uri.https("https://pokeapi.co/api/v2/pokemon/ditto");
-    http.get(url).then((value) {
-      if (value.statusCode == 200) {
-        var decodeData = jsonDecode(value.body);
-        var pokepedia = decodeData["abilities	"];
+  @override
+  Widget build(BuildContext context) {
+    final String name = pokemon['name'] ?? 'Unknown';
+    final String image = pokemon['image'] ?? '';
+    final List types = pokemon['types'] ?? [];
 
-        setState(() {});
-      }
-    });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name[0].toUpperCase() + name.substring(1)),
+        backgroundColor: _getTypeColor(types.isNotEmpty ? types[0] : ''),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              imageUrl: image,
+              height: 200,
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Types',
+              // style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 10),
+            Wrap(
+              spacing: 10,
+              children: types.map<Widget>((type) {
+                return Chip(
+                  label: Text(
+                    type[0].toUpperCase() + type.substring(1),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: _getTypeColor(type),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/pokemon_details.dart
+++ lib/screens/pokemon_details.dart
@@ -1,15 +1,89 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class PokeDetails extends StatefulWidget {
-  const PokeDetails({super.key});
+class PokeDetails extends StatelessWidget {
+  final Map pokemon;
 
-  @override
-  State<PokeDetails> createState() => _PokeDetailsState();
-}
+  const PokeDetails({super.key, required this.pokemon});
 
-class _PokeDetailsState extends State<PokeDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final String name = pokemon['name'];
+    final String image = pokemon['image'];
+    final List types = pokemon['type'];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name[0].toUpperCase() + name.substring(1)),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Center(
+              child: Image.network(image, height: 200),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "Type(s): ${types.join(", ")}",
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            FutureBuilder(
+              future: fetchPokemonDetails(name),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return const Text("Failed to load details");
+                }
+
+                final data = snapshot.data as Map;
+                final abilities = data['abilities'] as List;
+                final experience = data['base_experience'] ?? 0;
+                final cry = data['cries']['latest'] ?? "";
+
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Base Experience: $experience",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Abilities: ${abilities.map((a) => a['ability']['name']).join(', ')}",
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(height: 20),
+                      if (cry.isNotEmpty)
+                        ElevatedButton(
+                          onPressed: () {
+                          },
+                          child: const Text("Play Cry"),
+                        ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Map> fetchPokemonDetails(String name) async {
+    final url = Uri.parse("https://pokeapi.co/api/v2/pokemon/$name");
+    final res = await http.get(url);
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    } else {
+      throw Exception("Failed to load Pokémon details");
+    }
   }
 }
